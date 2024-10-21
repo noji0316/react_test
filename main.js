cb53bc007eaf5a822b052ee4add21a649d3c7de9
Caleb <njh2694@gmail.com>
2024-10-21 13:08:22 +0900

    첫 커밋

---
+++ main.js
@@ -0,0 +1,25 @@
+// console.log(a);
+// try {
+// console.log(b);
+// console.log(c);
+// } catch {};
+
+// var a = 1;
+// const b = 2;
+// let c;
+
+// function foo(arg) {
+//     return arg;
+//   }
+  
+//   function bar() {
+//     console.log('bar');
+//   }
+  
+//   foo(bar)();
+  
+const foo = function (arg) {
+    return arg;
+  }
+  
+  foo(1);
